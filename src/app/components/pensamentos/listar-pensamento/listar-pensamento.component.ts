3d0fe592a0b9ce4b1213b03296893ebad68c42a2
Jonas sousa <jonassousagtr@gmail.com>
2023-04-29 15:59:18 -0300

    feat: Criando a interação dos componentes e validações basicas na pag principal.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamento = [
+    {
+      conteudo: "teste",
+      autoria: "Jonas",
+      modelo: "modelo2"
+    },
+    {
+      conteudo: "testedasdasdasd",
+      autoria: "Jonas",
+      modelo: "modelo3"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
