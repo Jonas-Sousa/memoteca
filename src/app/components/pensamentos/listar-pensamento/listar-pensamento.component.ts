955ec8c8a832ab0a37d0ae0ec66d9ad77239a215
Jonas sousa <jonassousagtr@gmail.com>
2023-04-29 16:20:45 -0300

    styled: Colocando uma classe para alterar dinâmicamente os tamanhos dos cards conforme a quantidade de caracteres.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,15 +9,21 @@ export class ListarPensamentoComponent implements OnInit {
 
   listaPensamento = [
     {
-      conteudo: "teste",
+      conteudo: "testdfgdfgdfgdsdfsdfsdfsdfsdfsdfgdfgdfgdfgdfgde",
       autoria: "Jonas",
       modelo: "modelo2"
     },
     {
-      conteudo: "testedasdasdasd",
+      conteudo: "Passo informações para o e vou colocando o texto aqui ate coisa as coisa componete filho se aqui coisa ac coisa",
       autoria: "Jonas",
       modelo: "modelo3"
-    }
+    },
+    {
+      conteudo: "Lorem ipsum dolor sit amet consectetur adipisicing b explicabo sed voluptates, animi tempora autem ai voi coisano as coisa de uma forma que a coisa qui se coise mais coisas e quando coisa ele vai coisa a coisa coisada de um jeito coisa vou coisa mais coisa ate q a coisa coise ",
+      autoria: "Jonas",
+      modelo: "modelo1"
+    },
+
   ]
 
   constructor() { }
