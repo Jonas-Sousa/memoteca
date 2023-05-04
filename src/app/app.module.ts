fde714b120378a2bba1ef9616cc235aa621cdf28
Jonas sousa <jonassousagtr@gmail.com>
2023-05-03 21:21:22 -0300

    Feat: Criação do componente 'botão-carregar-mais' para carregar os cards da pag conforme a necessidade.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+import { BotaoCarregarMaisComponent } from './components/pensamentos/listar-pensamento/botao-carregar-mais/botao-carregar-mais.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { EditarPensamentoComponent } from './components/pensamentos/editar-pensa
     ListarPensamentoComponent,
     PensamentoComponent,
     ExcluirPensamentoComponent,
-    EditarPensamentoComponent
+    EditarPensamentoComponent,
+    BotaoCarregarMaisComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,11 +9,23 @@ import { Pensamento } from '../pensamento';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamento: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamento) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamento) => {
       this.listaPensamento = listaPensamento
+    })
+  };
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual)
+    .subscribe(listaPensamentos => {
+      this.listaPensamento.push(...listaPensamentos)
+      if(!listaPensamentos.length) {
+        this.haMaisPensamentos = false
+      }
     })
   }
 
