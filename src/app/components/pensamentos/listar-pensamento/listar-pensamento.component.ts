c63fb5dfdfac03baedb5d191a7e69b29c932dd71
Jonas sousa <jonassousagtr@gmail.com>
2023-05-03 21:50:05 -0300

    Feat: Inserindo a funcionalidade de filtro dos cards e atualizando a lista do db.json.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,22 +11,33 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamento: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = ''
+
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamento) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamento) => {
       this.listaPensamento = listaPensamento
     })
   };
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual)
+    this.service.listar(++this.paginaAtual, this.filtro)
     .subscribe(listaPensamentos => {
       this.listaPensamento.push(...listaPensamentos)
       if(!listaPensamentos.length) {
         this.haMaisPensamentos = false
       }
     })
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro)
+      .subscribe(listaPensamentos => {
+        this.listaPensamento = listaPensamentos
+      })
   }
 
 }
